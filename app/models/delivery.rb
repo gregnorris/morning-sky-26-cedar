c597a2764c7de94e5f42cad85095f35d5bc8fe1a
norris <user@ubuntu810desktop.localdomain>
2009-12-08 05:40:01 -0600

    changed editing of delivered items to be nested with editing a delivery

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -3,7 +3,8 @@ class Delivery < ActiveRecord::Base
   has_many :delivered_items, :foreign_key => :delivery_id
   belongs_to :recipient
   
-  #accepts_nested_attributes_for :delivered_items, :allow_destroy => true, :reject_if => :all_blank
+  accepts_nested_attributes_for :delivered_items, :allow_destroy => true, :reject_if => proc { |attributes| attributes.all? {|k,v| v.blank?} }
+
   
   ENTERED = 0
   PARTIAL = 1
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,9 @@ class Item < ActiveRecord::Base
     ITEM_SPACER.first(ITEM_SPACER.size - self.item_code.size)
   end
 
-  def self.items_for_select
-    Item.all.map { |it| ["#{it.item_code} #{it.dashes_string} #{it.name}", it.id]}
+  def self.items_for_select(add_or_select='select')
+    the_prompt_text = add_or_select == 'select' ? "Select an item" : "Add an item"
+    Item.all.map { |it| ["#{it.item_code} #{it.dashes_string} #{it.name}", it.id]}.unshift([the_prompt_text, nil])
   end
 
 end
