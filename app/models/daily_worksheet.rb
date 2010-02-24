b157f6de796a0f0b5fd11a9a9b68cba6e90c94e0
norris <user@ubuntu810desktop.localdomain>
2010-02-24 11:07:50 -0600

    FIXED: sytax error in daily_worksheet.rb

--- app/models/daily_worksheet.rb
+++ app/models/daily_worksheet.rb
@@ -2,6 +2,6 @@ class DailyWorksheet < ActiveRecord::Base
 
   has_many :daily_deliveries
   
-  named_scope :for_date,  lambda{ |a_date| {:conditions => ["worksheet_date = :term", {:term => "%#{a_date}%"}]}}
+  named_scope :for_date, lambda{ |a_date| {:conditions => ["worksheet_date = :term", {:term => "%#{a_date}%"}]}}
 
-  
+end
